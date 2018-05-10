692b8af3d2776bae372245cdb44387373a5fd01b
Derrick Awuah <dawuah3@gmail.com>
2018-05-10 18:09:13 -0400

    Fixed the onclick

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ window.onload = function () {
 
     var drawer = document.getElementById('drawer');
     // var menu = document.getElementsByClassName('nav_button');
-    var menu = document.querySelector('.nav_button');
+    var menu = document.querySelector('.nav_hamburger');
 
 
     menu.addEventListener('click', function (e) {
-        alert("hello");
-        // drawer.classList.toggle('open');
-        // e.stopPropagation();
+        // alert("hello");
+        drawer.classList.toggle('open');
+        e.stopPropagation();
     });
 };
